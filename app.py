dac333a19d495e11fdc3bef98c1880a05015a901
N3rdMusical05 <jp3188619@gmail.com>
2025-05-27 11:05:52 -0300

    aprendendo git

---
+++ app.py
@@ -0,0 +1,4 @@
+numb1 = int(input('Digite o primeiro número: '))
+numb2 = int(input('Digite o segundo número: '))
+soma  = numb1 + numb2
+print('O resultado da soma entre {} e {} é {}!'.format(numb1, numb2, soma))
