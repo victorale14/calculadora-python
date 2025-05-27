e09b84df52bfcb4ace6f738c8381a287bff883b6
salsapunk <bernardinhoagamespray@gmail.com>
2025-05-27 11:21:11 -0300

    lendo o arquivo de teste

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-numb1 = int(input('Digite o primeiro número: '))
-numb2 = int(input('Digite o segundo número: '))
-soma  = numb1 + numb2
-print('O resultado da soma entre {} e {} é {}!'.format(numb1, numb2, soma))
+f = open('teste123.txt', 'r')
+arquivo = f.read()
+print(arquivo)
