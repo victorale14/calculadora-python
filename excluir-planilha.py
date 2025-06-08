0aeb7d63c4c0adbd4e60dba7c41822f12860d588
Paulo Henrique Estevam <paulohenriqueestevam2@gmail.com>
2025-06-07 23:12:55 -0300

    Delete excluir-planilha.py

--- excluir-planilha.py
+++
@@ -1,9 +0,0 @@
-import os;
-import shutil;
-from tkinter.filedialog import askdirectory;
-nome_do_arquivo = askdirectory(title="Selecione o arquivo Excel que deseja excluir");
-try:
-    shutil.rmtree(nome_do_arquivo);
-    print(f"Arquivo {nome_do_arquivo} excluído com sucesso!");
-except FileNotFoundError:
-    print(f"Erro: Arquivo {nome_do_arquivo} não encontrado.");
